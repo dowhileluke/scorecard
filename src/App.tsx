febc71850a0d57e0f80bbd53d1a7a814e4e562c1
Luke Davis <dowhileluke@gmail.com>
2023-08-02 21:30:05 -0700

    replaced layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Viewport } from './components/viewport'
-import { Layout } from './components/layout'
 import { useSettings } from './hooks/useSettings'
 import { Grid } from './components/grid'
 import { Player } from './components/player'
@@ -8,23 +7,21 @@ export function App() {
   const [settings, setSettings] = useSettings()
   const { isInverted } = settings
 
-  console.log(settings)
-
-  const topGrid = (
-    <Grid templateCols="1fr auto 1fr">
-      <Player name={isInverted ? settings.p2 : settings.p1} isFirst />
-      <div style={{ width: '1rem', }} />
-      <Player name={isInverted ? settings.p1 : settings.p2} />
-    </Grid>
-  )
-
   return (
-    <Viewport>
-      <Layout
+    <Viewport className="blacken">
+      {/* <Layout
         className="blacken"
         top={topGrid}
         bot={<div>bot</div>}
-      />
+      /> */}
+      <Grid fullHeight templateRows="1fr auto">
+        <Grid templateCols="1fr auto 1fr">
+          <Player name={isInverted ? settings.p2 : settings.p1} isFirst />
+          <div style={{ width: '1rem', }} />
+          <Player name={isInverted ? settings.p1 : settings.p2} />
+        </Grid>
+        buttons
+      </Grid>
     </Viewport>
   )
 }
--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,19 +1,23 @@
 import { CSSProperties, PropsWithChildren } from 'react'
 import classes from './grid.module.css'
+import { ClassNamed } from '../types';
+import { merge } from '../functions/merge';
 
 type GridProps = {
+	fullHeight?: boolean;
 	templateCols?: string;
 	templateRows?: string;
 }
 
-export function Grid({ templateCols, templateRows, children }: PropsWithChildren<GridProps>) {
-	const gridStyle: CSSProperties = {
-		gridTemplateColumns: templateCols,
-		gridTemplateRows: templateRows,
-	}
+export function Grid({ fullHeight = false, templateCols, templateRows, className, children }: PropsWithChildren<GridProps & ClassNamed>) {
+	const gridStyle: CSSProperties = {}
+
+	if (fullHeight) gridStyle.height = '100%'
+	if (templateCols) gridStyle.gridTemplateColumns = templateCols
+	if (templateRows) gridStyle.gridTemplateRows = templateRows
 
 	return (
-		<div className={classes.grid} style={gridStyle}>
+		<div className={merge(classes.grid, className)} style={gridStyle}>
 			{children}
 		</div>
 	)
