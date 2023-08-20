7286c7949faebe35eb436d347d9c34f235155b60
Luke Davis <dowhileluke@gmail.com>
2023-08-20 11:48:07 -0700

    empty state

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,6 +9,8 @@ import { EditPlayerModal } from './modals/edit-player-modal'
 import { AmendPointsModal } from './modals/amend-points-modal'
 import { ActionButtons } from './action-buttons'
 import { ManagePlayersModal } from './modals/manage-players-modal'
+import { Button } from './primitives/interactive'
+import { Users } from 'react-feather'
 
 export function App() {
 	const [gameState, game] = useGameState()
@@ -19,14 +21,23 @@ export function App() {
 			<div className="col-group trim">
 				{gameState.players.map(p => (
 					<div key={p.id} className="app-col" style={{ flexShrink: p.name.length }}>
-						<Editable className="mza heading" onClick={() => setMode.editPlayer(p)}>
-							<div className={concat('fancy', p.name.length < 3 && 'brief')}>
+						<Editable className="mza font-2x" onClick={() => setMode.editPlayer(p)}>
+							<div className={concat('fancy underline', p.name.length < 3 && 'brief')}>
 								{p.name}
 							</div>
 						</Editable>
 						<ScoreTable points={gameState.scores[p.id]} onClick={index => setMode.amendScore(p, index)}/>
 					</div>
 				))}
+				{gameState.players.length === 0 && (
+					<div className="empty-col centered">
+						<div className="fancy font-2x">Nobody is here...</div>
+						<Button onClick={setMode.manage}>
+							<Users size="1.25em" /> Add Players
+						</Button>
+						<div>and get the party started!</div>
+					</div>
+				)}
 			</div>
 			<div className="col-group">
 				<PointsForm
